1631164b291852e97d00ae152b10a745a541cb92
maissa-gallah <maissaglh@gmail.com>
2022-01-18 19:08:17 +0100

    nav bar modified and footer too

--- src/app/home/categories/categories.component.ts
+++ src/app/home/categories/categories.component.ts
@@ -9,7 +9,7 @@ export class CategoriesComponent implements OnInit {
   
   categories = [
   
-    {id: 1,name:' Textbooks',des:'fact that a reader will be distracted by the readable content of a page when looking at its layout.Thepoint of using',imagePath:'cat1.png'},
+    {id: 1,name:' Novel',des:'fact that a reader will be distracted by the readable content of a page when looking at its layout.Thepoint of using',imagePath:'cat1.png'},
     {id: 2,name:' Science',des:'fact that a reader will be distracted by the readable content of a page when looking at its layout.Thepoint of using',imagePath:'cat2.png'},
     {id: 3,name:'  History',des:'fact that a reader will be distracted by the readable content of a page when looking at its layout.Thepoint of using',imagePath:'cat3.png'},
     {id: 4,name:' Biography',des:'fact that a reader will be distracted by the readable content of a page when looking at its layout.Thepoint of using',imagePath:'cat4.png'},
