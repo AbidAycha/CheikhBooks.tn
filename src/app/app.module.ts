62eb0ce874cda63438ced48a10f85211a4c0ee9b
MayssaJaz <missajaziri@gmail.com>
2022-01-18 13:34:31 +0100

    category-page-npot-done-yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CardBookComponent } from './card-book/card-book.component';
+import { CategoryPageComponent } from './books-page/category-page/category-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardBookComponent,
+    CategoryPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card-book/card-book.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-book',
+  templateUrl: './card-book.component.html',
+  styleUrls: ['./card-book.component.css']
+})
+export class CardBookComponent implements OnInit {
+  isHover:Boolean
+  @Input() index:number;
+  constructor() { 
+    this.isHover=false;
+    this.index=0;
+  }
+
+  ngOnInit(): void {
+  }
+  mouseLeaveAction():void{
+    this.isHover=false;
+  }
+  mouseEnterAction():void{
+    console.log("indeeex",this.index)
+    this.isHover=true
+  }
+}
