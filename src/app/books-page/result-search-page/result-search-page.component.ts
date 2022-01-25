d8829b6c9edc69a67ce0de43d100f12d03427d43
MayssaJaz <missajaziri@gmail.com>
2022-01-25 13:43:31 +0100

    added-filters-functionality

--- src/app/books-page/result-search-page/result-search-page.component.ts
+++ src/app/books-page/result-search-page/result-search-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute,Params } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { BookDto } from 'src/app/DTO/book.dto';
 import { BooksServiceService } from 'src/app/Services/books-service.service';
 
@@ -11,26 +11,33 @@ import { BooksServiceService } from 'src/app/Services/books-service.service';
 export class ResultSearchPageComponent implements OnInit {
   showButton: boolean;
   showFilters: boolean;
-  listOfBooks:any;
+  listOfBooks: any;
+  search: string;
 
-  constructor(private activatedRoute: ActivatedRoute,private serviceBook:BooksServiceService) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private serviceBook: BooksServiceService
+  ) {
     this.showButton = true;
     this.showFilters = false;
-    this.listOfBooks=[];
+    this.listOfBooks = [];
+    this.search = '';
   }
   showHideFilters(showFilterStatus: boolean) {
     this.showFilters = showFilterStatus;
   }
+  applyRemoveFilter(filteredData: any) {
+    this.listOfBooks = filteredData;
+  }
 
   ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe((params: Params) => {
+      this.search = params['search'];
       this.serviceBook
         .searchBookByTitleOrAuthor(params['search'])
         .subscribe((data) => {
           this.listOfBooks = data;
         });
-
-
     });
   }
 }
